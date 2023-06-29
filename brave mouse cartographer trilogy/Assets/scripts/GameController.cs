927c0238e4ecb38523872b964229ea6daf24cdec
uctuviun <gusboehling@gmail.com>
2023-06-29 17:41:57 -0400

    first pass at setting up title menu screen

---
+++ brave mouse cartographer trilogy/Assets/scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+
+	public void StartBMC1(){
+		SceneManager.LoadScene(1);
+	}
+
+	public void StartBMC2(){
+		SceneManager.LoadScene(2);
+	}
+
+	public void StartBMC3(){
+		SceneManager.LoadScene(3);
+	}
+}
