3f0b1f8b8a356384e4a27b61f40d53b1f406d541
uctuviun <gusboehling@gmail.com>
2024-01-31 14:54:00 -0500

    set up chong

---
+++ brave mouse cartographer trilogy/Assets/scripts/Chong.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Chong : MonoBehaviour
+{
+    public TextMeshProUGUI TextBoxes;
+    Coroutine ChongRoutine;
+
+    private void Awake()
+    {
+        ChongRoutine = StartCoroutine(ChongTime());
+    }
+
+    IEnumerator ChongTime()
+    {
+        yield return new WaitForSeconds(1.6f);
+        TextBoxes.SetText("C");
+        yield return new WaitForSeconds(.08f);
+        TextBoxes.SetText("CO");
+        yield return new WaitForSeconds(.08f);
+        TextBoxes.SetText("COM");
+        yield return new WaitForSeconds(.08f);
+        TextBoxes.SetText("COMM");
+        yield return new WaitForSeconds(.08f);
+        TextBoxes.SetText("COMMO");
+        yield return new WaitForSeconds(.08f);
+        TextBoxes.SetText("COMMON");
+        yield return new WaitForSeconds(.08f);
+        TextBoxes.SetText("COMMON ");
+        yield return new WaitForSeconds(.08f);
+        TextBoxes.SetText("COMMON O");
+        yield return new WaitForSeconds(.08f);
+        TextBoxes.SetText("COMMON OP");
+        yield return new WaitForSeconds(.08f);
+        TextBoxes.SetText("COMMON OPE");
+        yield return new WaitForSeconds(.08f);
+        TextBoxes.SetText("COMMON OPER");
+        yield return new WaitForSeconds(.08f);
+        TextBoxes.SetText("COMMON OPERA");
+        yield return new WaitForSeconds(.6f);
+        ChongRoutine = null;
+        SceneManager.LoadScene("title");
+    }
+}
